ef3483ac0142b814f84af800f23680351c4d9f7b
bzhezlo <bzhezlo@twilio.com>
2021-08-31 16:37:56 +0300

    wire things together: - persistent rates service - periodic polling of rates

--- forex-mtl/src/main/scala/forex/Main.scala
+++ forex-mtl/src/main/scala/forex/Main.scala
@@ -3,6 +3,9 @@ package forex
 import cats.effect._
 import forex.config._
 import fs2.Stream
+import org.http4s.client.Client
+import org.http4s.client.blaze.BlazeClientBuilder
+import org.http4s.client.middleware.{ Retry, RetryPolicy }
 import org.http4s.server.blaze.BlazeServerBuilder
 
 import scala.concurrent.ExecutionContext
@@ -11,7 +14,6 @@ object Main extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] =
     new Application[IO].stream(executionContext).compile.drain.as(ExitCode.Success)
-
 }
 
 class Application[F[_]: ConcurrentEffect: Timer] {
@@ -19,11 +21,25 @@ class Application[F[_]: ConcurrentEffect: Timer] {
   def stream(ec: ExecutionContext): Stream[F, Unit] =
     for {
       config <- Config.stream("app")
-      module = new Module[F](config)
-      _ <- BlazeServerBuilder[F](ec)
-            .bindHttp(config.http.port, config.http.host)
-            .withHttpApp(module.httpApp)
-            .serve
+      httpClient <- oneFrameHttpClient(ec, config.oneFrameRatesProvider)
+      module = new Module[F](config, httpClient)
+      _ <- module.ratesLoader.load
+      _ <- httpServer(ec, config, module)
     } yield ()
+
+  private def httpServer(ec: ExecutionContext, config: ApplicationConfig, module: Module[F]): Stream[F, ExitCode] =
+    BlazeServerBuilder[F](ec)
+      .bindHttp(config.http.port, config.http.host)
+      .withHttpApp(module.httpApp)
+      .serve
+
+  private def oneFrameHttpClient(ec: ExecutionContext, config: OneFrameRatesProviderConfig): Stream[F, Client[F]] = {
+    val retryPolicy =
+      RetryPolicy[F](RetryPolicy.exponentialBackoff(maxWait = config.retryDuration, maxRetry = config.maxRetries))
+    BlazeClientBuilder[F](ec)
+      .withRequestTimeout(config.http.timeout)
+      .stream
+      .map(client => Retry[F](retryPolicy)(client))
+  }
 
 }
