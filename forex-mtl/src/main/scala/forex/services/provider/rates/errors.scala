b33e0b8380b6f5b4bbbb0e02f5477bed7b3c2e17
bzhezlo <bzhezlo@twilio.com>
2021-09-07 00:37:15 +0300

    fix compilation error

--- forex-mtl/src/main/scala/forex/services/provider/rates/errors.scala
+++ forex-mtl/src/main/scala/forex/services/provider/rates/errors.scala
@@ -1,10 +1,12 @@
 package forex.services.provider.rates
 
+import scala.util.control.NoStackTrace
+
 object errors {
 
-  sealed trait Error
+  sealed abstract class Error(val message: String) extends Throwable(message) with NoStackTrace
   object Error {
-    final case class UriNotParseable(message: String) extends Error
-    final case class FetchRatesFailed(message: String) extends Error
+    final case class UriNotParseable(override val message: String) extends Error(message)
+    final case class FetchRatesFailed(override val message: String) extends Error(message)
   }
 }
